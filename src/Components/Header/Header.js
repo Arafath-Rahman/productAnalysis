481a6ab06175bb58bc4c208f1df986a3a5c69376
Arafath-Rahman <mkhan172093@bscse.uiu.ac.bd>
2022-04-04 13:30:47 +0600

    added route, custom link, designed navbar

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import CustomLink from '../CustomLink/CustomLink';
 import './Header.css';
 
 const Header = () => {
   return (
     <div>
       <nav>
-        <ul>
-          <li><Link to='/home'>HOME</Link></li>
-          <li><Link to='/reviews'>REVIEWS</Link></li>
-          <li><Link to='/dashboard'>DASHBOARD</Link></li>
-          <li><Link to='/blogs'>BLOGS</Link></li>
-          <li><Link to='/about'>ABOUT</Link></li>
+        <ul className='flex justify-center h-10 items-center bg-cyan-300 text-fuchsia-400 font-semibold'>
+          <li><CustomLink to='/home'>HOME</CustomLink></li>
+          <li><CustomLink to='/reviews'>REVIEWS</CustomLink></li>
+          <li><CustomLink to='/dashboard'>DASHBOARD</CustomLink></li>
+          <li><CustomLink to='/blogs'>BLOGS</CustomLink></li>
+          <li><CustomLink to='/about'>ABOUT</CustomLink></li>
         </ul>
       </nav>
     </div>
